db7be029530a75b3d1901b7394662767583680f9
Charles-Muia <Charles.K.Muia@gmail.com>
2023-08-29 16:24:54 +0300

    BBC_Lifestyle

--- home.php
+++ home.php
@@ -22,57 +22,153 @@
 <!--===BOOTSTRAP CDN--------------------------------------------------------------------------------------------------->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 
+
+<!--===GOOGLE FONTS---------------------------------------------------------------------------------------------------->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Dosis&display=swap" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bricolage+Grotesque:opsz,wght@10..48,800&family=Dosis&display=swap" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bricolage+Grotesque:opsz,wght@10..48,800&family=Dosis&family=Orbitron:wght@800;900&display=swap" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bricolage+Grotesque:opsz,wght@10..48,800&family=Dosis&family=Montserrat&family=Orbitron:wght@800;900&display=swap" rel="stylesheet">
+
 </head>
 <body>
 
-<!--===NAVBAR---------------------------------------------------------------------------------------------------------->
+<!--===NAVBAR SECTION---------------------------------------------------------------------------------------------------------->
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-        <div class="container">
-            <a class="navbar-brand" href="#">
-                <img src="Pics/BBC_Lifestyle_Logo_2.png" alt="Logo" class="w-50" id="BBC_Logo">
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav ms-auto mb-2 mg-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+            <div class="container">
+                <a class="navbar-brand" href="#">
+                    <img src="Pics/BBC_Lifestyle_Logo_2.png" alt="">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto mb-2 mg-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="#">Home</a>
+                        </li>
 
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">about</a>
-                    </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">about</a>
+                        </li>
 
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">our service</a>
-                    </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">our services</a>
+                        </li>
 
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">our work</a>
-                    </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">our work</a>
+                        </li>
 
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">blog</a>
-                    </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">blog</a>
+                        </li>
 
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">faqs</a>
-                    </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">faqs</a>
+                        </li>
 
-                    <li class="nav-item">
-                        <a class="nav-link btn text-white bg-dark px-3 rounded-0" href="#">contact us</a>
-                    </li>
+                        <li class="nav-item">
+                            <a class="nav-link btn text-white bg-dark px-3 rounded-0" href="#">contact us</a>
+                        </li>
+                </div>
+            </div>
+        </nav>
+<br>
+
+<!--===HERO SECTION---------------------------------------------------------------------------------------------------------->
+
+<!--        <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">-->
+<!--                    <div class="carousel-indicators">-->
+<!--                        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>-->
+<!--                        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>-->
+<!--                        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>-->
+<!--                    </div>-->
+<!--            <div class="carousel-inner">-->
+<!--                    <div class="carousel-item active">-->
+<!--                        <img src="Pics/CDWM_ZA3_Use_2.png" class="d-block w-100" alt="...">-->
+<!--                            <div class="carousel-caption carousel">-->
+<!--                                <p>1 / 3</p>-->
+<!--                                <h5>new season: <br> come dine with me </h5>-->
+<!--                            </div>-->
+<!--                    </div>-->
+<!---->
+<!--                    <div class="carousel-item">-->
+<!--                        <img src="Pics/CDWM_ZA3_Use_2.png" class="d-block w-100" alt="...">-->
+<!--                            <div class="carousel-caption carousel">-->
+<!--                                <p>2 / 3</p>-->
+<!--                                <h5>new season: <br> come dine with me </h5>-->
+<!--                            </div>-->
+<!--                    </div>-->
+<!---->
+<!--                    <div class="carousel-item">-->
+<!--                        <img src="Pics/CDWM_ZA3_Use_2.png" class="d-block w-100" alt="...">-->
+<!--                            <div class="carousel-caption carousel">-->
+<!--                                <p>3 / 3</p>-->
+<!--                                <h5>new season: <br> come dine with me </h5>-->
+<!--                            </div>-->
+<!--                    </div>-->
+<!---->
+<!--            </div>-->
+<!--                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">-->
+<!--                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>-->
+<!--                    <span class="visually-hidden">Previous</span>-->
+<!--                </button>-->
+<!--                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">-->
+<!--                    <span class="carousel-control-next-icon" aria-hidden="true"></span>-->
+<!--                    <span class="visually-hidden">Next</span>-->
+<!--                </button>-->
+<!--        </div>-->
+
+<div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+        <div class="carousel-item active" data-bs-interval="10000">
+            <img src="Pics/CDWM_ZA3_Use_2.png" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+                <p>1 / 3</p>
+                <h5>new season: <br> come dine with me </h5>
             </div>
         </div>
-    </nav>
-
-
-
-
-
-
+        <div class="carousel-item" data-bs-interval="2000">
+            <img src="Pics/CDWM_ZA2.jpg" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+                <p>2 / 3</p>
+                <h5>Second slide label</h5>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img src="Pics/MC.jpg" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+                <p>3 / 3</p>
+                <h5 class="master-chef">master chef: <br> the professionals</h5>
+            </div>
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+</div>
 
 <!--===JS-------------------------------------------------------------------------------------------------------------->
 <!--===BOOTSTRAP------------------------------------------------------------------------------------------------------->
