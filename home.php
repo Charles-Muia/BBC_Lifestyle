75e37962292c421a753d77d803a207d1cdb57887
Charles-Muia <Charles.K.Muia@gmail.com>
2023-08-29 11:46:22 +0300

    BBC_Lifestyle

--- home.php
+++ home.php
@@ -1,5 +1,5 @@
 <?php
-?>
+//?>
 
 <!doctype html>
 <html lang="en">
@@ -8,9 +8,75 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>BBC Lifestyle | Home</title>
+
+<!--===FAVICON--------------------------------------------------------------------------------------------------------->
+    <link rel="icon" href="Pics/BBC_Lifestyle_Logo.jpg">
+
+<!--===CSS------------------------------------------------------------------------------------------------------------->
+    <link rel="stylesheet" href="style.css">
+
+<!--===FONT-AWESOME CDN------------------------------------------------------------------------------------------------>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+
+<!--===BOOTSTRAP CDN--------------------------------------------------------------------------------------------------->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+
 </head>
 <body>
+
+<!--===NAVBAR---------------------------------------------------------------------------------------------------------->
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="Pics/BBC_Lifestyle_Logo_2.png" alt="Logo" class="w-50" id="BBC_Logo">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mg-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">about</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">our service</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">our work</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">blog</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">faqs</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link btn text-white bg-dark px-3 rounded-0" href="#">contact us</a>
+                    </li>
+            </div>
+        </div>
+    </nav>
+
+
+
+
+
+
+
+<!--===JS-------------------------------------------------------------------------------------------------------------->
+<!--===BOOTSTRAP------------------------------------------------------------------------------------------------------->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 
 </body>
 </html>
