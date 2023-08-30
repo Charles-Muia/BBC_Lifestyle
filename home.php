4e1908aa31fd7f15f3e40750e97396066180b2f1
Charles-Muia <Charles.K.Muia@gmail.com>
2023-08-30 14:48:46 +0300

    BBC_Lifestyle

--- home.php
+++ home.php
@@ -222,7 +222,7 @@
 
                     <div class="col">
                         <div class="card border-0 rounded-0">
-                            <img src="Pics/Listing_J.jpg" class="card-img-top rounded-0" alt="">
+                            <img src="Pics/ComeDineWithMe_Edited_2.jpg" class="card-img-top rounded-0" alt="">
                             <div class="card-body">
                                 <p class="card-text text-muted">listing jozi <span>season 2</span> is back exclusive to <br> BBC Lifestyle</p>
 
