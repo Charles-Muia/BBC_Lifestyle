d521d094a7b2730dd3be83999fcceac3b17b42a0
Charles-Muia <Charles.K.Muia@gmail.com>
2023-09-06 15:43:26 +0300

    BBC_Lifestyle

--- contact.php
+++ contact.php
@@ -76,98 +76,154 @@
 
 <!--===NAVBAR SECTION-------------------------------------------------------------------------------------------------->
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-    <div class="container">
-        <a class="navbar-brand" href="#">
-            <img src="Pics/BBC_Lifestyle_Logo_2.png" alt="">
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav ms-auto mb-2 mg-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="home.php">Home</a>
-                </li>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+            <div class="container">
+                <a class="navbar-brand" href="#">
+                    <img src="Pics/BBC_Lifestyle_Logo_2.png" alt="">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto mb-2 mg-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="home.php">Home</a>
+                        </li>
 
-                <li class="nav-item">
-                    <a class="nav-link" href="home.php#about-sec">about</a>
-                </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="home.php#about-sec">about</a>
+                        </li>
 
-                <li class="nav-item">
-                    <a class="nav-link" href="home.php#services-sec">our services</a>
-                </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="home.php#services-sec">our services</a>
+                        </li>
 
-                <li class="nav-item">
-                    <a class="nav-link" href="home.php#work-sec">our work</a>
-                </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="home.php#work-sec">our work</a>
+                        </li>
 
-                <li class="nav-item">
-                    <a class="nav-link" href="home.php#blog-sec">blog</a>
-                </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="home.php#blog-sec">blog</a>
+                        </li>
 
-                <li class="nav-item">
-                    <a class="nav-link" href="home.php#faqs">faqs</a>
-                </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="home.php#faqs">faqs</a>
+                        </li>
 
-                <li class="nav-item" >
-                    <a class="nav-link btn text-white bg-dark px-3 rounded-0" href="contact.php">contact us</a>
-                </li>
-        </div>
-    </div>
-</nav>
-<br>
-
-<!--===FOOTER SECTION ACCORDION---------------------------------------------------------------------------------------->
-
-<section>
-    <footer class="bg-dark bg-footer" style="margin-top: 170px">
-        <div class="container pt-5 pb-4" style="margin-top: 50px">
-            <div class="row">
-                <div class="col-lg-6">
-                    <h5 class="text-light title">about us</h5>
-                    <p class="text-light para">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                        Cum cupiditate enim illo libero, nam nisi omnis qui quisquam quo unde.</p>
-                    <p class="text-light number"><i class="fa fa-location"></i> 123 456 987</p>
-                    <p class="text-light number"><i class="fa fa-phone"></i> (+) 0123 456 987</p>
-                    <p class="text-light number"><i class="fa fa-envelope"></i> info@bbclifestyle.com</p>
-                </div>
-
-                <div class="col-lg-3">
-                    <h5 class="text-light title">quick links</h5>
-                    <ul class="text text-links">
-                        <li><a href="">home</a></li>
-                        <li><a href="">about</a></li>
-                        <li><a href="">services</a></li>
-                        <li><a href="">work</a></li>
-                        <li><a href="">blog</a></li>
-                        <li><a href="">faqs</a></li>
-                        <li><a href="">contact</a></li>
-                    </ul>
-                </div>
-
-                <div class="col-lg-3 text-dark social">
-                    <h5 class="text-light title">our socials</h5>
-                    <ul class="text text-social">
-                        <li><a href="#"><i class="fa-brands fa-facebook"></i></a></li>
-                        <li><a href="#"><i class="fa-brands fa-youtube"></i></a></li>
-                        <li><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
-                        <li><a href="#"><i class="fa-brands fa-linkedin"></i></a></li>
-                        <li><a href="#"><i class="fa-brands fa-pinterest"></i></a></li>
-                    </ul>
-                </div>
-
-            </div>
-            <br>
-            <hr class="line">
-            <div class="row">
-                <div class="col-lg-12">
-                    <p class="text-center text-light">Copyright &copy; 2023 . <a href="#">BBC Lifestyle</a> . All Rights Reserved</p>
+                        <li class="nav-item" >
+                            <a class="nav-link btn text-white bg-dark px-3 rounded-0" href="contact.php">contact us</a>
+                        </li>
                 </div>
             </div>
-        </div>
-    </footer>
-</section>
+        </nav>
+
+                    <br>
+
+<!--===CONTACT SECTION------------------------------------------------------------------------------------------------->
+
+        <section>
+            <div class="container mb-5">
+                <div class="col-md-12 col-lg-8" style="margin-top: 170px">
+                    <h1 class="text-single text-contact pt-5">contact us</h1>
+                </div>
+            </div>
+
+                <div class="contact mt-5">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-7">
+
+                                <form action="">
+                                    <div class="row g3">
+                                            <div class="col-md-6">
+                                                <label id="lab-txt" for="your-firstname" class="form-label" >your first name</label>
+                                                <input type="text" class="form-control" id="your-firstname" name="your-firstname" required placeholder=" Enter Your First Name">
+                                            </div>
+                                                <div class="col-md-6">
+                                                    <label id="lab-txt" for="your-secondname" class="form-label lab-text" >your second name</label>
+                                                    <input type="text" class="form-control" id="your-secondname" name="your-secondname" required placeholder="Enter Your Second Name">
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <label id="lab-txt" for="your-emailaddress" class="form-label lab-text" >your email address</label>
+                                                    <input type="email" class="form-control" id="your-emailaddress" name="your-emailaddress" required placeholder="Enter Your Email Address">
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <label id="lab-txt" for="your-sujbect" class="form-label lab-text" >your subject</label>
+                                                    <input type="text" class="form-control" id="your-subject" name="your-subject" required placeholder="Subject">
+                                                </div>
+                                                <div class="col-md-12">
+                                                    <label id="lab-txt" for="your-message" class="form-label lab-text" >your subject</label>
+                                                    <textarea name="your-message" id="your-message" rows="8" class="form-control" required placeholder="Type Your Message Here ........................ "></textarea>
+                                                </div>
+
+                                                        <div class="mt-3 mb-5">
+                                                            <button class="btn text-light bg-dark px-3 btn-lg button-message rounded-0" type="submit" style="font-weight: 600">send message</button>
+                                                        </div>
+                                    </div>
+                                </form>
+
+                            </div>
+
+                                    <div id="map" class="col-lg-5 mb-4 contact-map">
+                                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d75993.08981299611!2d-2.305863127226094!3d53.47231928828991!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487a4d4c5226f5db%3A0xd9be143804fe6baa!2sManchester%2C%20UK!5e0!3m2!1sen!2ske!4v1694002013582!5m2!1sen!2ske"
+                                                width="550" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
+                                        </iframe>
+                                    </div>
+                        </div>
+                    </div>
+                </div>
+        </section>
+
+<!--===FOOTER SECTION-------------------------------------------------------------------------------------------------->
+
+        <section>
+            <footer class="bg-dark bg-footer" style="margin-top: 170px">
+                <div class="container pt-5 pb-4" style="margin-top: 50px">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <h5 class="text-light title">about us</h5>
+                            <p class="text-light para">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                Cum cupiditate enim illo libero, nam nisi omnis qui quisquam quo unde.</p>
+                            <p class="text-light number"><i class="fa fa-location"></i> 123 456 987</p>
+                            <p class="text-light number"><i class="fa fa-phone"></i> (+) 0123 456 987</p>
+                            <p class="text-light number"><i class="fa fa-envelope"></i> info@bbclifestyle.com</p>
+                        </div>
+
+                        <div class="col-lg-3">
+                            <h5 class="text-light title">quick links</h5>
+                            <ul class="text text-links">
+                                <li><a href="">home</a></li>
+                                <li><a href="">about</a></li>
+                                <li><a href="">services</a></li>
+                                <li><a href="">work</a></li>
+                                <li><a href="">blog</a></li>
+                                <li><a href="">faqs</a></li>
+                                <li><a href="">contact</a></li>
+                            </ul>
+                        </div>
+
+                        <div class="col-lg-3 text-dark social">
+                            <h5 class="text-light title">our socials</h5>
+                            <ul class="text text-social">
+                                <li><a href="#"><i class="fa-brands fa-facebook"></i></a></li>
+                                <li><a href="#"><i class="fa-brands fa-youtube"></i></a></li>
+                                <li><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
+                                <li><a href="#"><i class="fa-brands fa-linkedin"></i></a></li>
+                                <li><a href="#"><i class="fa-brands fa-pinterest"></i></a></li>
+                            </ul>
+                        </div>
+
+                    </div>
+                    <br>
+                    <hr class="line">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <p class="text-center text-light">Copyright &copy; 2023 . <a href="#">BBC Lifestyle</a> . All Rights Reserved</p>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </section>
 
 <!--===JS-------------------------------------------------------------------------------------------------------------->
 <!--===BOOTSTRAP------------------------------------------------------------------------------------------------------->
